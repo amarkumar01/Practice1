bb0da79e533e11f1997321ff83d951eb954d58ab
Amar kumar <amarkumar@192.168.1.133>
2026-07-13 20:28:08 +0100

    excercise 2nd and 3rd has completed

---
+++ learn_the_python_hard_way_solved/numbers_and_maths.py
@@ -0,0 +1,16 @@
+#These are variables
+a=21567
+b=52
+c=9.223
+d=8876
+
+##starting with add
+add = a+b+c+d
+print("addition of all the variables are", add)
+
+##Now subtraction
+subtraction = a - (b*c) + d
+print("subtraction of variables are being used in this formula", subtraction)
+
+print("a is greater then b is", a > b)
+print("a divided by d is", a/d )
---
+++ learn_the_python_hard_way_solved/print_and_comment.py
@@ -0,0 +1,18 @@
+#/usr/bin/python
+
+#This is a commented line. This will not be printed.(IMP:- This is just a line)
+
+'''
+Multiple lines commented
+This is multi line comment
+It comes under 3 single quoted
+'''
+
+#Now adding print statements
+
+#print a string
+print("This is my 1st day of coding practice")
+#print a number
+print(85)
+print("Printing a sentence")
+print(32.12)
