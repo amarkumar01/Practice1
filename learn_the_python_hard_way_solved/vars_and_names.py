5b4b2610cab1a9710dc6a94972c894f805b252c3
Amar kumar <amarkumar@192.168.1.133>
2026-07-13 20:40:00 +0100

    excercise 4 completed

---
+++ learn_the_python_hard_way_solved/vars_and_names.py
@@ -0,0 +1,7 @@
+name = "amar"
+age = 33
+work = "Sr SRE Engineer"
+car_owned = False
+water_consume_everyday = 4.5
+
+print("Hi my name is", name, "and i am", age, "years old.", "I work as a", work, ". I own a car statement is", car_owned, "And i drink almost", water_consume_everyday, "Liters of water.")
