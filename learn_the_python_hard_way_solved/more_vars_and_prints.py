a46930b30f28e9e307c3817af32d635263ad862f
Amar kumar <amarkumar@192.168.1.133>
2026-07-14 23:41:32 +0100

    excercise 4 completed

---
+++ learn_the_python_hard_way_solved/more_vars_and_prints.py
@@ -0,0 +1,12 @@
+#/usr/bin/python3
+
+my_name = "amar k,"
+my_all_name = "amar, chhotu, etc"
+my_full_name = "Amar kumar"
+
+print("My all name are", my_name, my_all_name, "and my Full Name is", my_full_name)
+
+#printing once again
+
+#print("My all name are" % (my_name, my_all_name) "and my Full Name is" % my_full_name)
+print(f"My names are {my_name}, {my_all_name}, and my full name is {my_full_name}")
